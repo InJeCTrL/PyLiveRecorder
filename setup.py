e773abaeebaf0041bb8d24f51c030187d371dd5d
InJeCTrL <dap1933@hotmail.com>
2020-07-28 14:58:05 +0800

    setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyliverecorder',
+    version=__import__("PyLiveRecorder").__version__,
+    packages=find_packages(),
+    description='Monitor the start of live and download media stream automatically.',
+    author='InJeCTrL',
+    author_email='dap1933@hotmail.com',
+    license='MIT',
+    url='https://github.com/InJeCTrL/PyLiveRecorder/',
+    install_requires=[
+        "requests",
+        "bs4"
+        ]
+    )
