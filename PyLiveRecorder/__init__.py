70838fad4424af4c50ddbb2c6115280f427838d1
InJeCTrL <dap1933@hotmail.com>
2020-08-04 00:14:21 +0800

    change ffmpeg command about downloading hls

--- PyLiveRecorder/__init__.py
+++ PyLiveRecorder/__init__.py
@@ -51,7 +51,7 @@ class Monitor:
         try:
             # download HLS video depending on external FFmpeg
             if self.__liveurl.endswith(".m3u8") or self.__liveurl.find(".m3u8?") != -1:
-                proc = subprocess.Popen("\"ffmpeg.exe\" -loglevel quiet -headers \"User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:76.0) Gecko/20100101 Firefox/76.0\" " + 
+                proc = subprocess.Popen("\"ffmpeg\" -loglevel quiet -headers \"User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:76.0) Gecko/20100101 Firefox/76.0\" " + 
                                     "-i \"%s\" %s" % (self.__liveurl, self.__filename), 
                                     shell = False, stdout = subprocess.DEVNULL, 
                                     stderr = subprocess.DEVNULL, stdin = subprocess.PIPE)
